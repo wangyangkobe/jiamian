078727ddcb1411b29d3e3a045ecad4a5eb934ae1
wangyang <25001860@qq.com>
2014-08-24 21:33:33 +0800

    增加类 CategoryCell。

---
+++ JiaMian/CategoryCell.h
@@ -0,0 +1,14 @@
+//
+//  CategoryCell.h
+//  JiaMian
+//
+//  Created by wanyang on 14-8-24.
+//  Copyright (c) 2014年 wy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoryCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+@end
---
+++ JiaMian/CategoryCell.m
@@ -0,0 +1,22 @@
+//
+//  CategoryCell.m
+//  JiaMian
+//
+//  Created by wanyang on 14-8-24.
+//  Copyright (c) 2014年 wy. All rights reserved.
+//
+
+#import "CategoryCell.h"
+
+@implementation CategoryCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+@end
